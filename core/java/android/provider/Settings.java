bf07a0d498778a868938625f32bab3490136f086
Lars Greiss <kufikugel@googlemail.com>
2014-12-21 01:07:30 -0700

    Frameworks: add abillity to use quick settings pulldown on the left side
    This commit gives the ability to use quick settings pulldown on the left
    or right edge of the statusbar. As well this respects still RTL layouts
    where it logically should change to the oposite side.
    
    Change-Id: Ibdf0894347e1ed3871f685afd5a29f47efe23a4b
    (cherry picked from commit 250b89410ce990af28873753c10fdc616acd5587)
    
    (cherry picked from commit 21e4cc8cd6a46182a0e6406c393e3466d5dc6204)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2084,12 +2084,6 @@ public final class Settings {
          */
         public static final String BUTTON_BACKLIGHT_TIMEOUT = "button_backlight_timeout";
 
-        /** Whether to allow one finger quick settings expansion on the right side of the statusbar.
-         *
-         * @hide
-         */
-        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
-
         /**
          * Control whether the process CPU usage meter should be shown.
          *
@@ -8111,6 +8105,12 @@ public final class Settings {
          * @hide
          */
         public static final String FORMAL_TEXT_INPUT = "formal_text_input";
+
+        /**
+         * Whether to allow one finger quick settings expansion on the right side of the statusbar.
+         * @hide
+         */
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
     }
 
