f90f7e035a972b703d3e00e08f2128fe98463d88
hany <hany.alsamman@gmail.com>
2014-12-25 14:20:25 -0700

    XuiMod: Toast Animations [1/2]
    Add Xylon, Xperia Z1, TokoROM Transition and more
    
    https://github.com/zst123/XuiMod/commit/8fc1152fb485ed5e007ac8d4ea9ad7b7f1c493d8
    
    https://github.com/zst123/XuiMod/commit/1989a90850e5491799620e1f86fcbc9ae54f5161
    
    https://github.com/zst123/XuiMod/commit/e3383f127244822b9fc61db169b59e4a502d127b
    
    Credit zst123, thanks for GwonHyeok
    
    Change-Id: I50b278a61148acfdd253e9770c6651217fc749bf
    (cherry picked from commit 5021e97306ca81468d6d72ca23390f109582b4c2)
    
    Conflicts:
    	core/res/res/values/cr_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3279,6 +3279,12 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_MEDIA_STREAM = "volume_keys_control_media_stream";
 
         /**
+         * Toast Animations
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
